0c0cbf5452d72d8a94a6995435db29c4dc0631c0
William Loock <145708222+WXscho@users.noreply.github.com>
2025-02-28 21:38:08 -0600

    converson of timezone time to comparable string is complete. program on a basic level is complete.

--- emailScript.py
+++ emailScript.py
@@ -45,7 +45,6 @@ format_time = "%H:%M:%S"
 formated_set_date_time = set_date + " " + set_time
 
 #creating a function to assemble all email components
-
 def email_new(g_sender, g_recipients, g_subject, g_body, g_password):
   # lists off headings for the email in order they are inserted into the list
   e_msg = MIMEText(g_body) # creates message containing text
@@ -56,6 +55,7 @@ def email_new(g_sender, g_recipients, g_subject, g_body, g_password):
   context = ssl.create_default_context()
 
   try:
+    #starts the connection with the smtp_server for gmail
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
       # login for the SMTP server is the users gmail account
       server.login(g_sender, g_password)
@@ -66,16 +66,27 @@ def email_new(g_sender, g_recipients, g_subject, g_body, g_password):
     print(f" Exception: {e}")
 
 print(f"date and time: {datetime.now(pytz.timezone('US/Central'))}  formated_set_date_time: {formated_set_date_time}")
+
 while True:
+  # sleep(1) pauses the execution for one second
   sleep(1)
-  # to convert timezone into usable comparison format
-  print(datetime.now(pytz.timezone('US/Central')))
+  # todo convert timezone into usable comparison format
+
+  # timezone_date gets the date and time within the timezone
+  # timezone_time_string converts the timezone into a string
+  # timezone_time_string_format grabs only the date and time (excluding miliseconds and actual time)
+  # making it comparable to the users input
+  timezone_time = datetime.now(pytz.timezone('US/Central'))
+  timezone_time_string = str(timezone_time)
+  timezone_time_string_formated = timezone_time_string[:19]
+
+  print(f"time zone time: {timezone_time_string_formated}")
   print(formated_set_date_time)
-  if datetime.now(pytz.timezone('US/Central')) == formated_set_date_time:
+  if timezone_time_string_formated  == formated_set_date_time:
     email_new(sender, recipients, subject, body, password)
     break
 # email_new(sender, recipients, subject, body, password)
-#In both instances, Gmail will encrypt
+# In both instances, Gmail will encrypt
 # emails using TLS, as this is the more
 # secure successor of SSL. As per Python’s
 # Security considerations, it is highly
